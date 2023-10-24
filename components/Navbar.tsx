b74b667f55348cccd1e74bb86544726be4940ddf
swastik <swastikpatel29@gmail.com>
2023-10-24 16:15:54 +0530

    implemented Dark Mode.

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,38 +1,90 @@
 'use client'
 
 import React from 'react'
+import Image from 'next/image';
 import { Link} from 'react-scroll'
 
+import { ThemeToggler } from './ThemeToggle'
+
+import {db ,auth} from '@/utils/firebase';
+import {useAuthState} from 'react-firebase-hooks/auth';
+
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
 function Navbar() {
+
+  const [user , loading] = useAuthState(auth);
+
   return (
     <>
     
-      <nav className='flex flex-row justify-center gap-36 mt-4' data-aos="fade-down"
+      <nav className='flex flex-row justify-between mx-auto max-w-7xl gap-36 mt-4' data-aos="fade-down"
      data-aos-easing="linear"
      data-aos-duration="1500">
-      <a href="/">
-        <img src="/scholar.svg" alt="" className='w-24 h-24 -mt-4 hover:scale-125 transition' />
+
+      <div>
+        <a href="/">
+          <img src="/scholar.svg" alt="" className='w-24 h-24 -mt-4 hover:scale-125 transition' />
         </a>
-        <ul className='flex flex-row space-x-12 justify-center mt-6'>
-        
-      <li className='cursor-pointer hover:scale-125 transition hover:text-gray-600 hover:font-bold'>
-        <Link to='/'>Home</Link>
-        </li>
-      <li className='cursor-pointer hover:scale-125 transition hover:text-gray-600 hover:font-bold'>
-       <Link to='cards' smooth={true} duration={1000} > Events </Link>
-        </li>
-      <li className='cursor-pointer hover:scale-125 transition hover:text-gray-600 hover:font-bold'>
-        <Link to='about' smooth={true} duration={1000}>About</Link>
-        </li>
-      <li className='cursor-pointer hover:scale-125 transition hover:text-gray-600 hover:font-bold'>
-       <Link to='feature' smooth={true} duration={1000}> Features </Link>
-        </li>
-        <li className='cursor-pointer hover:scale-125 transition hover:text-gray-600 hover:font-bold'>
-       <Link to='newsletter' smooth={true} duration={1000}> Newsletter </Link>
-        </li>
+      </div>
       
-    </ul>
-    <button className='bg-[#9333EA] p-2 h-14 mt-2 rounded-full px-8 text-white hover:bg-purple-700'>Sign Up</button>
+
+      <div>
+          <ul className='flex flex-row space-x-12 justify-center mt-6'>
+            
+            <li className='cursor-pointer hover:scale-125 transition hover:text-gray-600 hover:font-bold'>
+              <Link to='/'>Home</Link>
+              </li>
+            <li className='cursor-pointer hover:scale-125 transition hover:text-gray-600 hover:font-bold'>
+            <Link to='cards' smooth={true} duration={1000} > Events </Link>
+              </li>
+            <li className='cursor-pointer hover:scale-125 transition hover:text-gray-600 hover:font-bold'>
+              <Link to='about' smooth={true} duration={1000}>About</Link>
+              </li>
+            <li className='cursor-pointer hover:scale-125 transition hover:text-gray-600 hover:font-bold'>
+            <Link to='feature' smooth={true} duration={1000}> Features </Link>
+              </li>
+              <li className='cursor-pointer hover:scale-125 transition hover:text-gray-600 hover:font-bold'>
+            <Link to='newsletter' smooth={true} duration={1000}> Newsletter </Link>
+              </li>
+            
+          </ul>
+      </div>
+
+
+      <div className=' flex gap-4 '>
+          <ThemeToggler className='mt-5'/>
+          {user ? (
+          // <Image className='rounded-full' src={user.photoURL} width={40} height={40} alt='user'/>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Avatar>
+                  <AvatarImage src={user.photoURL!} />
+                  <AvatarFallback>CN</AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+          <DropdownMenuContent>
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className=' text-red-600' onClick={() => auth.signOut()}>Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+
+          </DropdownMenu>          
+          ) : (
+            <button className='bg-[#9333EA] p-2 h-14 mt-2 rounded-full px-8 text-white hover:bg-purple-700'>Sign Up</button>
+          )}
+      </div>  
+     
     </nav>
    
     </>
